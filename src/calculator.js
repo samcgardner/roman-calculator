d0b1b436df0aa7d418d23ca31ff3da2fb38a6be9
Sam Gardner <samcgardner@gmail.com>
2018-11-05 00:27:44 +0000

    Add check to try and handle javascript that ends up not evaluating to an int sanely

--- src/calculator.js
+++ src/calculator.js
@@ -7,5 +7,8 @@ module.exports = function (input) {
     parsedInput = input.replace(numeralRegex, (string) => romans.toArabic(string))
     inputWithExponentsReplaced = parsedInput.replace(exponentRegex, '**')
     result = eval(inputWithExponentsReplaced)
+    if (!Number.isInteger(result)) {
+        return 'Sorry, it looks like you passed in an invalid calculation string'
+    }
     return romans.toRoman(result)
 };
